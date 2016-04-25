f0d91032c556bb4474e3553facc1bf95497b25d4
andimalik <andi.malik.notifications@gmail.com>
2016-04-25 15:01:51 +0700

    Fixed typo in hibernate configuration.

--- src/main/java/com/andimalik/tasklister/App.java
+++ src/main/java/com/andimalik/tasklister/App.java
@@ -1,6 +1,7 @@
 package com.andimalik.tasklister;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -21,12 +22,21 @@ public class App {
 		TaskService taskService = (TaskService) App.applicationContext
 				.getBean("taskService");
 
-		Task task = new Task();
-		task.setBeginTime(new Date());
-		task.setEndTime(new Date());
-		task.setTaskDescription("Description");
-		task.setTaskName("Task Name");
+		Task newTask = new Task();
+		newTask.setBeginTime(new Date());
+		newTask.setEndTime(new Date());
+		newTask.setTaskDescription("Description");
+		newTask.setTaskName("Task Name");
 
-		taskService.save(task);
+		taskService.save(newTask);
+
+		List<Task> list = taskService.getTasks();
+
+		for (Task task : list) {
+			System.out.println(task.getTaskName() + " ["
+					+ task.getBeginTime().toString() + "] #"
+					+ task.getId().toString() + "\n" + "    "
+					+ task.getTaskDescription());
+		}
 	}
 }
